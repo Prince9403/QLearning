8d1d5b9a89641fa756c88af87c94f1eff01ced78
Prince9403 <vasily.kuznets@gmail.com>
2026-01-25 18:30:52 +0200

    feat: added QLearning for stones game

---
+++ qlearning_in_stones_game.py
@@ -0,0 +1,55 @@
+import typing as t
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from stones_game_policy import StonesGame, StonesGamePolicy, EpsGreedyStonesGamePolicy
+
+
+def qlearning_for_policy(pol: StonesGamePolicy, num_stones: int, alpha: float, num_games: int):
+    gamma = pol.gamma
+    game = pol.game
+
+    for i in range(num_games):
+        curr_num_stones = num_stones
+        gamer_idx = 0
+        while curr_num_stones > 0:
+            next_num_stones = pol.next_state(curr_num_stones, gamer_idx)
+            action = curr_num_stones - next_num_stones
+
+            if next_num_stones > 0:
+                rew = 0
+            elif gamer_idx == 0:
+                rew = 1.0
+            else:
+                rew = -1.0
+
+            gamer_idx = 1 - gamer_idx
+
+            if next_num_stones == 0:
+                game.dct_returns[(curr_num_stones, action)] = (1 - alpha) * game.dct_returns[(curr_num_stones, action)] + alpha * rew
+            else:
+                _, best_q = game.get_best_action(next_num_stones, gamer_idx)
+                game.dct_returns[(curr_num_stones, action)] += alpha * (rew + gamma * best_q - game.dct_returns[(curr_num_stones, action)])
+            curr_num_stones = next_num_stones
+
+
+if __name__ == "__main__":
+    game = StonesGame([1, 2, 3])
+
+    pol = EpsGreedyStonesGamePolicy(game, 0.1, 0.1, 1.0)
+    qlearning_for_policy(pol, 20, 0.3, 200000)
+
+    num_stones = 20
+    gamer_idx = 0
+
+    print(f"Number of stones; {num_stones}")
+
+    while num_stones > 0:
+        best_action, _ = game.get_best_action(num_stones, gamer_idx)
+        print(f"{num_stones} stones. Player {gamer_idx} took {best_action} stones, remained {num_stones - best_action} stones")
+        num_stones = num_stones - best_action
+        gamer_idx = 1 - gamer_idx
+
+
+
---
+++ stones_game_policy.py
@@ -0,0 +1,48 @@
+import random
+from abc import ABC, abstractmethod
+from collections import defaultdict
+
+import numpy as np
+
+
+class StonesGame:
+    def __init__(self, possible_stones_number):
+        self.possible_stones_number = possible_stones_number
+        self.dct_returns = defaultdict(float)
+
+    def get_best_action(self, stones_number, gamer_idx):
+        possible_actions = [num for num in self.possible_stones_number if num <= stones_number]
+        rews = defaultdict(float)
+        for action in possible_actions:
+            rews[action] = self.dct_returns[(stones_number, action)]
+        if gamer_idx == 0:
+            key_best = max(rews, key=rews.get)
+        else:
+            key_best = min(rews, key=rews.get)
+        return key_best, rews[key_best]
+
+    def get_random_action(self, stones_number):
+        possible_actions = [num for num in self.possible_stones_number if num <= stones_number]
+        return random.choice(possible_actions)
+
+
+class StonesGamePolicy(ABC):
+    @abstractmethod
+    def next_state(self, stones_number, gamer_idx):
+        pass
+
+
+class EpsGreedyStonesGamePolicy(StonesGamePolicy):
+    def __init__(self, game: StonesGame, epsilon0: float, epsilon1: float, gamma: float):
+        super().__init__()
+        self.game = game
+        self.epsilons = np.array([epsilon0, epsilon1])
+        self.gamma = gamma
+
+    def next_state(self, stones_number, gamer_idx):
+        p = np.random.uniform(low=0, high=1)
+        if p < self.epsilons[gamer_idx]:
+            return stones_number - self.game.get_random_action(stones_number)
+        else:
+            best_act, best_reward = self.game.get_best_action(stones_number, gamer_idx)
+            return stones_number - best_act
